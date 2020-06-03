2f70365502be4a1ecd620bf565e3c70c23fd65a3
Rodrigo Lucas <rodrigollrdglucas@gmail.com>
2020-06-03 19:19:44 -0300

    start

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const routes = require('./routs')
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 
-app.listen(3333)
+app.listen(3333);
